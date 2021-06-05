3e3d777dd9b982083376f687286524ea3f7920c2
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-06-05 17:17:25 +0200

    Additional styling, added some keyboard functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Button({
   id,
@@ -152,7 +152,170 @@ function Button({
         "Don't end or start your calculations with an operator... Make sure the calculations ends or starts with a number."
       );
     }
+
+    // Create another function that will be used to use the calculator not by clicking, but rather by pressing the right number on the keyboard
   }
+
+  function handleKeyDown(e) {
+    const keyCodeArr = [
+      49, 50, 51, 52, 53, 54, 55, 56, 57, 8, 106, 107, 109, 111, 13, 190,
+    ];
+    e.preventDefault();
+    if (keyCodeArr.includes(e.keyCode)) {
+      value = e.key;
+      try {
+        switch (e.key) {
+          case "Backspace":
+            setCalculations("0");
+            setResult(0);
+            break;
+          case "/":
+            setCalculations((prevState) => {
+              let length = prevState.length;
+              if (
+                (prevState.split("")[length - 2] === "-" &&
+                  prevState.split("")[length - 5] === "/") ||
+                (prevState.split("")[length - 2] === "-" &&
+                  prevState.split("")[length - 5] === "+") ||
+                (prevState.split("")[length - 2] === "-" &&
+                  prevState.split("")[length - 5] === "*")
+              ) {
+                return prevState.slice(0, length - 6) + " / ";
+              }
+              if (
+                prevState.split("")[length - 2] === "/" ||
+                prevState.split("")[length - 2] === "*" ||
+                prevState.split("")[length - 2] === "-" ||
+                prevState.split("")[length - 2] === "+"
+              ) {
+                return prevState.slice(0, length - 3) + " / ";
+              }
+              return prevState + " / ";
+            });
+            setResult("/");
+            break;
+          case "*":
+            setCalculations((prevState) => {
+              let length = prevState.length;
+              if (
+                (prevState.split("")[length - 2] === "-" &&
+                  prevState.split("")[length - 5] === "/") ||
+                (prevState.split("")[length - 2] === "-" &&
+                  prevState.split("")[length - 5] === "+") ||
+                (prevState.split("")[length - 2] === "-" &&
+                  prevState.split("")[length - 5] === "*")
+              ) {
+                return prevState.slice(0, length - 6) + " * ";
+              }
+
+              if (
+                prevState.split("")[length - 2] === "/" ||
+                prevState.split("")[length - 2] === "*" ||
+                prevState.split("")[length - 2] === "-" ||
+                prevState.split("")[length - 2] === "+"
+              ) {
+                return prevState.slice(0, length - 3) + " * ";
+              }
+              return prevState + " * ";
+            });
+            setResult("*");
+            break;
+          case "-":
+            setCalculations((prevState) => {
+              let length = prevState.length;
+
+              if (
+                (prevState.split("")[length - 2] === "-" &&
+                  prevState.split("")[length - 5] === "/") ||
+                (prevState.split("")[length - 2] === "-" &&
+                  prevState.split("")[length - 5] === "+") ||
+                (prevState.split("")[length - 2] === "-" &&
+                  prevState.split("")[length - 5] === "*")
+              ) {
+                return prevState.slice(0, length - 6) + " - ";
+              }
+
+              if (prevState.split("")[length - 2] === "-") {
+                return prevState.slice(0, length - 3) + " - ";
+              }
+              return prevState + " - ";
+            });
+            setResult("-");
+            break;
+          case "+":
+            setCalculations((prevState) => {
+              let length = prevState.length;
+
+              if (
+                (prevState.split("")[length - 2] === "-" &&
+                  prevState.split("")[length - 5] === "/") ||
+                (prevState.split("")[length - 2] === "-" &&
+                  prevState.split("")[length - 5] === "+") ||
+                (prevState.split("")[length - 2] === "-" &&
+                  prevState.split("")[length - 5] === "*")
+              ) {
+                return prevState.slice(0, length - 6) + " + ";
+              }
+
+              if (
+                prevState.split("")[length - 2] === "/" ||
+                prevState.split("")[length - 2] === "*" ||
+                prevState.split("")[length - 2] === "-" ||
+                prevState.split("")[length - 2] === "+"
+              ) {
+                return prevState.slice(0, length - 3) + " + ";
+              }
+              return prevState + " + ";
+            });
+            setResult("+");
+            break;
+          case "Enter":
+            setCalculations(eval(calculations).toString());
+            setResult(eval(calculations));
+            break;
+          case ".":
+            if (result.includes(".")) return;
+            setCalculations((prevState) => prevState + ".");
+            setResult((prevState) => prevState + value);
+            break;
+          default:
+            setResult((prevState) => {
+              // Dont use strict equality with 0 for it to work
+              if (
+                prevState == 0 ||
+                prevState === "/" ||
+                prevState === "*" ||
+                prevState === "-" ||
+                prevState === "+"
+              ) {
+                return value;
+              }
+              return prevState + value;
+            });
+            setCalculations((prevState) => {
+              if (prevState == 0) {
+                return value;
+              }
+              return prevState + value;
+            });
+        }
+      } catch (err) {
+        console.log(err);
+        // Add material UI alert dialog component instead?
+        alert(
+          "Don't end or start your calculations with an operator... Make sure the calculations ends or starts with a number."
+        );
+      }
+    }
+  }
+
+  // Use useMemo() instead, so it doesnt get called multiple times??
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  });
 
   return (
     <button id={id} className={id} onClick={handleClick}>
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,18 @@
 import React from "react";
 
 function Display({ result, calculations }) {
+  // Inline style used to make the font size of result smaller over certain lengths, to make sure the result stays in one line
+  const fontSize =
+    result.toString().length > 18
+      ? { fontSize: "1.6rem" }
+      : { fontSize: "1.8rem" };
+
   return (
     <div className="display">
       <div>{calculations}</div>
-      <div id="display">{result}</div>
+      <div id="display" style={fontSize}>
+        {result}
+      </div>
     </div>
   );
 }
